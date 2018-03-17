e7ed622693058b75ed094d3c8baa22acb7e43774
Prem <myfrozenpark@gmail.com>
2018-03-17 20:14:05 +0545

    round pad pages added

---
+++ consumable-round-pads.php
@@ -0,0 +1,322 @@
+<?php include("includes/header.php"); ?>
+<section class="page-header">
+<div class="container">
+  <div class="row">
+    <div class="col s12">
+    <h3>Round Pads</h3>
+
+    </div>
+  </div>
+</div>
+</section>
+<section class="page company-profile">
+  <div class="container">
+    <div class="row page-content">
+<div class="col m4 s12">
+<?php include("includes/sidebar.php") ?>
+</div>
+<div class="col m8 s12">
+  <div class="content">
+<div class="row">
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_100.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 10 mm  B - 10  H - 31 mm ca Print Area  3 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.100 s</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_111.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 35 mm  B - 35  H - 34 mm ca Print Area  18 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.111</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_305.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 34 mm  B - 34  H - 41 mm ca Print Area  24 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.305</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_R403.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 58 mm  B - 58  H - 50 mm ca Print Area  45 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.R 403</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_503.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 94mm  B - 94  H - 53 mm ca Print Area  55 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.503</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_623.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 100mm  B -100  H-70 mm ca Print Area 85 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.623</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_900.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 118mm  B - 118  H - 78 mm ca Print Area  95 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.900</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_901.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 169 mm  B - 169  H - 90 mm ca Print Area  125 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.901</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_m57.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  -  59 mm  B - 59  H - 60 mm ca Print Area  25  <br>Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.M57</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP1.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L - 30 mm  B - 30  H - 47 mm ca Print Area  12 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP1</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP2.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 90mm  B - 90  H - 70 mm ca Print Area  55 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP2</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP3.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 50mm  B - 50  H - 50 mm ca Print Area  30 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP3</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP5.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  -  52 mm  B - 52  H - 75 mm ca Print Area  16 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP5</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP8.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L-287 mm B-20.3 H-24.7 mm ca Print Area 265x10</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP8</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP10.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L - 120mm  B - 120  H - 75 mmca Print Area  90 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP10</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP11.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 124 mm  B - 124  H - 72 mm ca Print Area  108 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP11</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP12.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L - 121mm  B - 121  H - 82 mm ca Print Area  115 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP12</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP16.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L - 37.7 mm  B - 37.7  H - 40 mm ca Print Area  19 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP16</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP18.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  -  128 mm  B - 128  H - 96 mm ca Print Area  100 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP18</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_RP19.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L - 34 mm  B - 34  H - 41.5 mm ca Print Area  28 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.RP19</a>
+          </div>
+        </div>
+  </div>
+
+  <div class=" col l4 m6 s12">
+    <div class="card">
+          <div class="card-image round-pad">
+            <img class="responsive-img materialboxed" src="images/consu_roundpad_T521.gif">
+          </div>
+          <div class="card-content mh-100">
+            <p>L  - 10 mm  B - 10  H - 31 mm ca Print Area  3 Ø</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Pad No.T 521</a>
+          </div>
+        </div>
+  </div>
+</div>
+  </div>
+</div>
+
+
+    </div>
+  </div>
+</section>
+<?php include("includes/footer.php"); ?>
