77b88cd905386081b1edb97156274aae352e3858
Prem <myfrozenpark@gmail.com>
2018-03-19 18:54:27 +0545

    Introduction to Digital Printing Laser Applications Laser Accessories pages added

---
+++ digital.php
@@ -0,0 +1,61 @@
+<?php include("includes/header.php"); ?>
+<section class="page-header">
+<div class="container">
+  <div class="row">
+    <div class="col s12">
+    <h3>Introductin To Digital Printing Solutions</h3>
+    </div>
+  </div>
+</div>
+</section>
+<section class="page company-profile">
+  <div class="container">
+    <div class="row page-content">
+<div class="col m4 s12">
+<?php include("includes/sidebar.php") ?>
+</div>
+<div class="col m8 s12">
+  <div class="content">
+<p class="text">Rita Pad Printing Systems Ltd. is pleased to announce the introduction of the RDM-17S Digital Printing Machine. The RDM 17S Digital Printing Machine represents a new concept in the field of printing, marking and decorating of products utilising a Computer- to- Product technique by using Rita Pad Printing Systems proprietory developed Digital inkjet technology.</p>
+<p class="text">The machine can print :</p>
+
+<ul class="decimal">
+  <li><p class="text">Logos</p></li>
+  <li><p class="text">Text</p></li>
+  <li><p class="text">Barcodes and Images with high resolution, precision and quality.</p></li>
+</ul>
+
+<h3 class="sub-title">Digital Printing</h3>
+
+<img class="responsive-img" src="images/digitalprinting.jpg" alt="">
+
+<p class="text">Offline and Online automations for the Digital Printing Machine are available best suited as per customer's requirements. The Digital Pad Printing Machine enables you to maintain consistent high quality print at the most economical cost.</p>
+
+<p class="text">The RDM-17S Digital Printing Machine is the right machine to print on Rigid PVC, Conduit pipes, Packing Straps, Caps, Bottles, Pouches, Laminates, Plastics, Tubes, Scales, Dials, Tetra Pack, Cartons, Labels etc. etc</p>
+
+<p class="text">If you require any clarification, please feel free to contact us.</p>
+
+<h3 class="sub-title">Download Brochure:</h3>
+
+<div class="card" style="max-width:200px;">
+        <div class="card-image">
+          <img class="responsive-img" src="images/downloads/digitalmachine1.png" alt="digitalmachine1">
+        </div>
+        <div class="card-action">
+          <a href="pdf/digitalmachine1.pdf" target="_blank">Download</a>
+        </div>
+      </div>
+
+      <h3 class="sub-title">Video</h3>
+
+      <div class="video-container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/cwLqqG0DhZM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+  </div>
+</div>
+
+
+    </div>
+  </div>
+</section>
+<?php include("includes/footer.php"); ?>
